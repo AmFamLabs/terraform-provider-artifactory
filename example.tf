28a74f0e2a83700097a6f5a912b38b8b3b4c1c2a
Joseph Chiocchi <jchiocch@amfam.com>
2020-04-28 18:41:22 -0500

    update example.tf with notes, use data.artifactory_artifact

--- example.tf
+++ example.tf
@@ -1,3 +1,14 @@
-resource "artifactory_artifact" "test_artifact" {
+provider "artifactory" {
+  url = "https://artifacts.amfamlabs.com"
+}
+
+// we only need READs for MVP, methinks
+data "artifactory_artifact" "test_artifact" {
   repository_path = "lambda/propinc/ingest/fake.zip"
 }
+
+// this is the terraform use case
+//resource "artifactory_artifact" "test_artifact" {
+//  repository_path = "lambda/propinc/ingest/fake.zip"
+//  body = ""
+//}
