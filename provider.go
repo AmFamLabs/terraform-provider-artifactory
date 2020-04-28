ef0dcb1d74b414e9662957dc2cd5edd2f139618b
Joseph Chiocchi <jchiocch@amfam.com>
2020-04-28 18:40:41 -0500

    update provider required input, add datasource to DataSourcesMap

--- provider.go
+++ provider.go
@@ -6,8 +6,18 @@ import (
 
 func Provider() *schema.Provider {
 	return &schema.Provider{
+		Schema: map[string]*schema.Schema{
+			"url": {
+				Type:        schema.TypeString,
+				Description: "The artifactory URL",
+				Required:    true,
+			},
+		},
 		ResourcesMap: map[string]*schema.Resource{
 			"artifactory_artifact": resourceArtifact(),
+		},
+		DataSourcesMap: map[string]*schema.Resource{
+			"artifactory_artifact": dataSourceArtifact(),
 		},
 	}
 }
