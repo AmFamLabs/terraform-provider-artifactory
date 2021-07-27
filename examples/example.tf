67af9cde5a76d2a2fa02a2fe7e10fe37ff05aa28
Sriharsha Karampudi <sriharsha@karampudi.me>
2021-07-27 10:09:23 -0400

    add example

--- examples/example.tf
+++ examples/example.tf
@@ -1,44 +1,31 @@
 provider "artifactory" {
-  url = "https://artifacts.amfamlabs.com"
+  url = "${var.artifactory_url}"
   s3_region = "us-east-1"
-  //will work :)
-  //assume_role {
-  //  //role_arn     = "arn:aws:iam::${data.consul_keys.config.var["${var.workspace}_id"]}:role/terraform_enterprise"
-  //  //role_arn     = "arn:aws:iam::some_id:role/terraform_enterprise"
-  //  role_arn = "arn:aws:iam::REDACTED:role/core-operator"
-  //  session_name = "artifactory"
-  //}
+  assume_role {
+    role_arn     = "${var.iam_role_arn}"
+    session_name = "artifactory"
 }
+
+// this is the terraform use case
+resource "artifactory_artifact_s3_deployment" "test_artifact" {
+  repository_path = "lambda/test-1.0.0.zip"
+  s3_bucket = "live-tmp-bucket"
+  s3_prefix = "lambda/deployments"
+  s3_region = "us-east-1"
+}
+
 
 // we only need READs for MVP, methinks
 data "artifactory_artifact" "test_artifact" {
   repository_path = "lambda/propinc/ingest/replicate-2.30.0.zip"
 }
 
-
-
-
-// this is the terraform use case
-resource "artifactory_artifact_s3_deployment" "test_artifact" {
-  repository_path = "lambda/propinc/ingest/replicate-2.30.0.zip"
-  s3_bucket = "yolk-propinc-live-tmp-bucket"
-  s3_prefix = "lambda/deployments"
-  s3_region = "us-east-1"
-}
-
-resource "artifactory_artifact_s3_deployment" "test_artifact_deux" {
-  repository_path = "lambda/propinc/ingest/${local.artifact_name}"
-  s3_bucket = "yolk-propinc-live-tmp-bucket"
-  s3_prefix = local.s3_prefix
-}
-
-
 locals {
   repo      = data.artifactory_artifact.test_artifact.repo
   path      = data.artifactory_artifact.test_artifact.path
   checksums = data.artifactory_artifact.test_artifact.checksums
-  s3_prefix = "lambda/deployments-deux"
-  artifact_name = "replicate-2.30.0.zip"
+  s3_prefix = "lambda/test-deployments"
+  artifact_name = "test-1.0.0.zip"
 }
 
 // this is the input
